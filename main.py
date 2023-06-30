c7b2305b39ef082aa803c270a7305c4952ca59c0
asamisea <138194179+asamisea@users.noreply.github.com>
2023-06-30 15:44:20 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+
+    info = Input()
+
+    coordinate = Face_recognition(info["num"]) 
+
+    Area_definition(coordinate, info)
+
+    while():
+        judge = Cheating_judgement()
+        if(judge == False):
+            suspect = Coordinate_judgement(judge)
+            Notification(suspect)
+        if(time == False):
+            break
